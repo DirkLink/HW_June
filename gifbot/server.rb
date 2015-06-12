56b4acea33a49975616dc2e2b457997bfda27287
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-12 14:12:03 -0400

    Clean up

--- gifbot/server.rb
+++ gifbot/server.rb
@@ -38,21 +38,9 @@ class GifBotWeb < Sinatra::Base
     t = gifitize.tag_gif params[:id], params[:tag_name]
     t.to_json
   end
-
-
-  # post "/add" do
-  #   listicize = ToDoList.new
-  #   item = listicize.create_entry params[:list], params[:description], params[:due_date]
-  #   item.id.to_s
-  # end
-
-  get "/list/:name" do
-    list = List.find_by_list_name! params[:name]
-    list.items.to_json
-  end
 end
 
 # $0 is $PROGRAM_NAME
 if $0 == __FILE__
-  ToDoWeb.start!
+  GifBotWeb.start!
 end
